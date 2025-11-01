44ae77a3bffe5dee1bde8165ed64d714f7229d42
Yusuf Akorede <koredey4u@gmail.com>
2025-11-01 10:26:05 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center flex-1 bg-white">
+      <Text className="text-5xl font-bold text-center font-quicksand">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
