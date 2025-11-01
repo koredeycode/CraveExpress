6a902a0f36576d4b74f9cd6f3443890365084c85
Yusuf Akorede <koredey4u@gmail.com>
2025-11-01 21:32:55 +0100

    implement zustand

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
+import useAuthStore from "@/store/auth.store";
 import { Redirect, Slot } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
-  const isAuthenticated = false;
+  const { isAuthenticated } = useAuthStore();
+
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
   return <Slot />;
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
+import useAuthStore from "@/store/auth.store";
+import * as Sentry from "@sentry/react-native";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import "./globals.css";
-import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
-  dsn: 'https://9f471eda041104407c53b50dd4c586ed@o4510288813686784.ingest.de.sentry.io/4510291046432848',
+  dsn: "https://9f471eda041104407c53b50dd4c586ed@o4510288813686784.ingest.de.sentry.io/4510291046432848",
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
@@ -17,13 +18,17 @@ Sentry.init({
   // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
 });
 
 export default Sentry.wrap(function RootLayout() {
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
@@ -37,6 +42,12 @@ export default Sentry.wrap(function RootLayout() {
 
     if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
+
+  useEffect(() => {
+    fetchAuthenticatedUser();
+  }, []);
+
+  if (!fontsLoaded || isLoading) return null;
 
   return <Stack screenOptions={{ headerShown: false }} />;
 });
