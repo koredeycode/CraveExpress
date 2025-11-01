2e6628d4c3926611052341270bb53600d6585219
Yusuf Akorede <koredey4u@gmail.com>
2025-11-01 11:05:39 +0100

    do home ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,84 @@
-import { Text, View } from "react-native";
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
+import cn from "clsx";
+import { Fragment } from "react";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function Index() {
   return (
-    <View className="items-center justify-center flex-1 bg-white">
-      <Text className="text-5xl font-bold text-center font-quicksand">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#FFFFFF22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="w-1/2 h-full">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="leading-tight text-white h1-bold">
+                        {item.title}
+                      </Text>
+                      <Image
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor={"FFFFFF"}
+                        source={images.arrowRight}
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        ListHeaderComponent={
+          <View className="flex-row w-full px-5 my-5 flex-between">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-row flex-center gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-black-100">Nigeria</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
+          </View>
+        }
+        contentContainerClassName="pb-20 px-5"
+      />
+    </SafeAreaView>
   );
 }
